8bc5b180db95e8772b5d3e8497ef1e60388c7259
Paolo Fontana <paolo.ftn95@gmail.com>
2024-07-17 09:57:47 +0200

    feat: added shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -62,12 +62,10 @@ const config = {
                     from: { height: 'var(--radix-accordion-content-height)' },
                     to: { height: '0' },
                 },
-              
             },
             animation: {
                 'accordion-down': 'accordion-down 0.2s ease-out',
                 'accordion-up': 'accordion-up 0.2s ease-out',
-
             },
         },
     },
